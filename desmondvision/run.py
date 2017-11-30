af96a3adb07ac0ae94d856f1fecbe2f59643d819
Kevin Chavez <kjchavez@stanford.edu>
2017-11-29 21:03:27 -0800

    A bit more config

--- desmondvision/run.py
+++ desmondvision/run.py
@@ -2,6 +2,7 @@ import argparse
 import cv2
 import logging
 import sys
+import time
 
 from desmond.perception import sensor
 from desmond import types
@@ -9,6 +10,11 @@ from desmond import types
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--name", help="Sensor name", default="Camera")
+    parser.add_argument("--show", '-s', action="store_true",
+                        help="Display images as they are captured.")
+    parser.add_argument("--min_period", '-p', type=int, default=25,
+                        help="Minimum delay (in milliseconds) between frames")
     return parser.parse_args()
 
 def main():
@@ -20,13 +26,13 @@ def main():
 
     s = sensor.Sensor("Camera")
     while True:
+        start = time.time()
         ret, frame = video_capture.read()
         if not ret:
             logging.debug("Failed to read frame.")
             continue
 
         ret, buf = cv2.imencode('.jpg', frame)
-
         if not ret:
             logging.debug("Failed to encode image.")
             continue
@@ -35,14 +41,19 @@ def main():
         image = types.Image(data=buf.tostring(), encoding=types.Image.JPEG)
         # Another copy...
         s.emit(image)  # And the copy to the socket buffer...
+        if args.show:
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(args.min_period) & 0xFF == ord('q'):
+                break
+        else:
+            try:
+                time.sleep(args.min_period / 1000.0 - (time.time() - start))
+            except KeyboardInterrupt:
+                break
 
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
 
     video_capture.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
